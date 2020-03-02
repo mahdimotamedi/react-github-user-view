38d1d4d02f267baad3d9bb558b0bc61de80e0e0a
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2020-03-02 21:18:51 +0330

    css and font files for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    js: ['babel-polyfill', './src/index.js'],
+    js: ["babel-polyfill", "./src/index.js"]
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -13,8 +13,16 @@ module.exports = {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
       },
       {
         test: /\.(png|jp(e*)g|svg)$/,
