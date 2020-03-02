2761bd9bbcbaea326d5a3d61022e88c6505a1727
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2020-03-02 13:00:02 +0330

    feat: add file loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,18 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: "images/[hash]-[name].[ext]"
+            }
+          }
+        ]
       }
     ]
   },
