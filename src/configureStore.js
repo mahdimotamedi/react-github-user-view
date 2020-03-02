51a4b8bcbfa260533b86f7b25b6e6f1629531035
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2020-03-02 17:53:07 +0330

    reformat

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
 import reducers from "./core/reducers/reducer.index";
 
 const loggerMiddleware = createLogger();
@@ -10,5 +10,5 @@ export default function configureStore(preloadedState) {
     reducers,
     preloadedState,
     applyMiddleware(thunkMiddleware, loggerMiddleware)
-  )
+  );
 }
