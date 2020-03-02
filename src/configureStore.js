448b913b108688cc9c83b378ef82849f54fe21fa
Mahdi Motamedi <mahdi.moatamedi@yahoo.com>
2020-03-02 17:52:37 +0330

    add config store file

---
+++ src/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import reducers from "./core/reducers/reducer.index";
+
+const loggerMiddleware = createLogger();
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    reducers,
+    preloadedState,
+    applyMiddleware(thunkMiddleware, loggerMiddleware)
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducers from "./core/reducers/reducer.index";
+import configureStore from "./configureStore";
 
 import "./assets/sass/index.scss";
 
-const store = createStore(reducers);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
